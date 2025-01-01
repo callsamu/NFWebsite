eef2213d18ef670a1488f610fad0d6cdaedb2d36
callsamu <samuelbree@gmail.com>
2025-01-01 14:35:20 -0300

    test: write case for basic editor functionality
    Ensures basic elements can be used via markdown shortcuts

--- src/components/ContentEditor/Editor.cy.jsx
+++ src/components/ContentEditor/Editor.cy.jsx
@@ -1,6 +1,34 @@
+const { Editor } = require("./Editor")
+
+function type(text) {
+	cy.get('@editor').type(text);
+}
+
 describe('<Editor />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-	  // cy.mount(<Editor />)
+  it('should support headings, marks and lists with markdown shortcuts', () => {
+	  cy.mount(<Editor />)
+
+	  cy.get('.tiptap').as('editor');
+
+	  type('# Heading 1{enter}');
+	  cy.get('@editor').find('h1').contains('Heading 1');
+
+	  type('## Heading 2{enter}');
+	  cy.get('@editor').find('h2').contains('Heading 2');
+
+	  type('Lorem ipsum{enter}');
+	  cy.get('@editor').find('p').contains('Lorem ipsum');
+
+	  type('- Item 1{enter}{enter}');
+	  cy.get('@editor').find('ul').first().contains('Item 1');
+
+	  type('paragraph with **bold**, _italic_ and ~~strike~~ text{enter}');
+	  cy.get('@editor').find('p').find('strong').contains('bold');
+	  cy.get('@editor').find('p').find('em').contains('italic');
+	  cy.get('@editor').find('p').find('s').contains('strike');
+
+	  type('1. numbered list{enter}second item{enter}{enter}');
+	  cy.get('@editor').find('ol').find('li').eq(0).contains('numbered list');
+	  cy.get('@editor').find('ol').find('li').eq(1).contains('second item');
   })
 })
