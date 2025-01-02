3900fb49038e17b61d2348087cb89f46ec3604ef
callsamu <samuelbree@gmail.com>
2025-01-02 09:57:55 -0300

    feat: add buttons to Editor toolbar
    Buttons for toggling lists, bolds, italics and strikes were added

--- src/components/ContentEditor/Editor.jsx
+++ src/components/ContentEditor/Editor.jsx
@@ -1,6 +1,20 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import "./Editor.scss";
+import { Bold, Italic, List, ListOrdered, Strikethrough } from "lucide-react";
+
+const ToggleButton = ({ active, children, ...buttonProps }) => {
+	return (
+		<button
+			type="button"
+			aria-selected={active}
+			className={`toggle-button ${active ? "active" : ""}`}
+			{...buttonProps}
+		>
+			{children}
+		</button>
+	);
+};
 
 /**
 * Rich text Editor Component
@@ -18,6 +32,45 @@ export const Editor = ({ content }) => {
 	return (
 		<div className="editor">
 			<div className="editor__toolbar">
+				<div>
+					<ToggleButton
+						title="Bold"
+						onClick={() => editor.chain().focus().toggleBold().run()}
+						active={editor.isActive("bold")}
+					>
+						<Bold size={16} />
+					</ToggleButton>
+					<ToggleButton
+						title="Italic"
+						onClick={() => editor.chain().focus().toggleItalic().run()}
+						active={editor.isActive("italic")}
+					>
+						<Italic size={16} />
+					</ToggleButton>
+					<ToggleButton
+						title="Strikethrough"
+						onClick={() => editor.chain().focus().toggleStrike().run()}
+						active={editor.isActive("strike")}
+					>
+						<Strikethrough size={16} />
+					</ToggleButton>
+				</div>
+				<div>
+					<ToggleButton
+						title="Ordered List"
+						onClick={() => editor.chain().focus().toggleOrderedList().run()}
+						active={editor.isActive("orderedList")}
+					>
+						<ListOrdered size={16} />
+					</ToggleButton>
+					<ToggleButton
+						title="List"
+						onClick={() => editor.chain().focus().toggleBulletList().run()}
+						active={editor.isActive("bulletList")}
+					>
+						<List size={16} />
+					</ToggleButton>
+				</div>
 			</div>
 			<div className="editor__container">
 				<EditorContent editor={editor} />
